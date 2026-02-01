92588ae05d6d8017df0af104ef3b509365d33b64
addiveloper <addi.veloper@gmail.com>
2026-02-01 18:36:30 -0400

    💄 adjust column responsive

--- src/app/(app)/favorites/page.tsx
+++ src/app/(app)/favorites/page.tsx
@@ -2,7 +2,7 @@ import { FavoritesSection } from "@/components/global/favorites-section";
 
 export default function Home() {
   return (
-    <section className="grid grid-cols-5 auto-rows-min gap-4 border border-muted-foreground/20 rounded-md w-full p-5">
+    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-min gap-4 border border-muted-foreground/20 rounded-md w-full p-5">
       <FavoritesSection />
     </section>
   );
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const tools = getTools();
 
   return (
-    <section className="grid grid-cols-5 auto-rows-min gap-4 border border-muted-foreground/20 rounded-md w-full p-5">
+    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-min gap-4 border border-muted-foreground/20 rounded-md w-full p-5">
       {tools.map((tool, index) => (
         <ToolCard key={index} tool={tool} />
       ))}
