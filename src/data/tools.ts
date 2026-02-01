92c8353088c85cb6a1248ea8a863eb350e4bb918
addiveloper <addi.veloper@gmail.com>
2026-02-01 16:11:04 -0400

    🚀 add initial tools

---
+++ src/data/tools.ts
@@ -0,0 +1,14 @@
+import { Tool } from "@/types/tool";
+
+export const tools: Tool[] = [
+  {
+    title: "Next.js",
+    category: ["Framework", "Backend", "Frontend", "React", "Web"],
+    url: "https://nextjs.org/",
+  },
+  {
+    title: "Tailwind CSS",
+    category: ["Frontend", "Web", "CSS"],
+    url: "https://tailwindcss.com/",
+  },
+];
